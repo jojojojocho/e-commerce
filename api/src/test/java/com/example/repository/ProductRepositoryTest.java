685b1f063d62e019be1edd71dd3b2c8091b138d6
jojojojocho <uuu9214@naver.com>
2023-02-21 17:09:05 +0900

    .....

---
+++ api/src/test/java/com/example/repository/ProductRepositoryTest.java
@@ -0,0 +1,11 @@
+package com.example.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductRepositoryTest {
+
+
+}
